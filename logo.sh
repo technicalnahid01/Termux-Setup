bebda4b128926af161cfe13c651ee4293d0b5973
technicalnahid01 <technicalnahid01@gmail.com>
2025-05-08 02:05:00 +0600

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -1,6 +1,14 @@
-#!/bin/bash                                          clear                                                                                                     # Colors
-RED='\033[1;31m'                                     GREEN='\033[1;32m'                                   YELLOW='\033[1;33m'
-BLUE='\033[1;34m'                                    CYAN='\033[1;36m'                                    WHITE='\033[1;37m'                                   NC='\033[0m' # No Color                                                                                   # Simple box design                                  echo -e "${CYAN}+===============================================+"
+#!/bin/bash                                          
+clear                                                                                                     
+# Colors
+RED='\033[1;31m'                                     
+GREEN='\033[1;32m'                                   
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'                                    
+CYAN='\033[1;36m'                                    
+WHITE='\033[1;37m'                                   
+NC='\033[0m' # No Color                                                                                   
+# Simple box design                                  
 echo -e "${CYAN}+===============================================+"
 echo -e "|                                               |"
 echo -e "|              ${YELLOW}TECHNICAL NAHID${CYAN}                  |"
